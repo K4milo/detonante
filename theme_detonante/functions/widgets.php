e99574a2249e5258472692d4b8ecf7882d6edd5f
k4milo <kmilo.vanegas@gmail.com>
2018-05-10 07:21:34 -0500

    Estilos de footer en su primera versión, se ñaden  nuevos widgets

--- theme_detonante/functions/widgets.php
+++ theme_detonante/functions/widgets.php
@@ -16,12 +16,57 @@ function bst_widgets_init() {
   ) );
 
   /*
+    Contact
+  */
+
+  register_sidebar( array(
+    'name'            => __( 'Widget Contacto', 'bst' ),
+    'id'              => 'widget-contact',
+    'description'     => __( 'Widget de contacto tranversal al sitio', 'bst' ),
+    'before_widget'   => '<section class="%1$s %2$s">',
+    'after_widget'    => '</section>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+  /*
   Footer (three widget areas)
    */
   register_sidebar( array(
     'name'            => __( 'Footer', 'bst' ),
     'id'              => 'footer-widget-area',
     'description'     => __( 'The footer widget area', 'bst' ),
+    'before_widget'   => '<div class="%1$s %2$s col-sm-4">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+
+  register_sidebar( array(
+    'name'            => __( 'Footer Columna 1', 'bst' ),
+    'id'              => 'footer-column-1',
+    'description'     => __( 'Primera columna de footer', 'bst' ),
+    'before_widget'   => '<div class="%1$s %2$s col-sm-4">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+  register_sidebar( array(
+    'name'            => __( 'Footer Columna 2', 'bst' ),
+    'id'              => 'footer-column-2',
+    'description'     => __( 'Segunda columna de footer', 'bst' ),
+    'before_widget'   => '<div class="%1$s %2$s col-sm-4">',
+    'after_widget'    => '</div>',
+    'before_title'    => '<h4>',
+    'after_title'     => '</h4>',
+  ) );
+
+  register_sidebar( array(
+    'name'            => __( 'Footer Columna 3', 'bst' ),
+    'id'              => 'footer-column-3',
+    'description'     => __( 'Tercera columna de footer', 'bst' ),
     'before_widget'   => '<div class="%1$s %2$s col-sm-4">',
     'after_widget'    => '</div>',
     'before_title'    => '<h4>',
--- theme_detonante/includes/footer.php
+++ theme_detonante/includes/footer.php
@@ -1,11 +1,26 @@
-<footer class="container site-footer">
-  <hr/>
-	<div class="row">
-    <?php dynamic_sidebar('footer-widget-area'); ?>
+<footer class="container-fluid site-footer">
+  
+  <div class="row contact-widget">
+     <?php dynamic_sidebar('widget-contact'); ?> 
   </div>
-  <hr/>
-  <div class="row">
-    <div class="col-lg-12 site-sub-footer">
+  
+
+  <div class="row footer-columns">
+    <div class="container">
+      
+        <?php dynamic_sidebar('footer-column-1'); ?>
+      
+      
+        <?php dynamic_sidebar('footer-column-2'); ?>
+      
+      
+        <?php dynamic_sidebar('footer-column-3'); ?>
+      
+    </div>
+  </div>
+  
+  <div class="row footer-copyright">
+    <div class="site-sub-footer">
       <p>&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></p>
     </div>
   </div>
