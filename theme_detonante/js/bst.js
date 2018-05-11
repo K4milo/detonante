c7242a885c9fea6046c3be708e5a117a6c9742b1
Camilo Vanegas <Camilo.Vanegas@vml.com>
2018-05-11 11:56:03 -0500

    Single page de emprendimiento

--- theme_detonante/js/bst.js
+++ theme_detonante/js/bst.js
@@ -2,17 +2,40 @@
 	
 	"use strict";
 
-	//Slider
-	var slider_wrapper = $('.hero-slider--main-slider');
+	//Slider  and coarousel scripts
+	var slider_wrapper = $('.hero-slider--main-slider'),
+		carousel_empr = $('.gallery-carousel');
 
-		if(slider_wrapper) {
-			//trigger the slider
-			slider_wrapper.slick({
-				dots: true,
-				infinite: false,
-				speed: 300
-			});
-		}
+	if(slider_wrapper) {
+		//trigger the slider
+		slider_wrapper.slick({
+			dots: true,
+			infinite: false,
+			speed: 300
+		});
+	}
+
+	if(carousel_empr) {
+		carousel_empr.slick({
+			dots: true,
+			infinite: false,
+			speed: 300,
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        infinite: true,
+		        dots: true
+		      }
+		    }
+		  ]
+		});
+	}
+
 	
 
 }(jQuery));
--- theme_detonante/single-emprendimiento.php
+++ theme_detonante/single-emprendimiento.php
@@ -1,9 +1,93 @@
 <?php
+
+/*
+	Variable fields
+
+	logo_field
+	url_page_field
+	items_contacto
+		contact_type
+		contact_text
+		vinculo_de_contacto
+	gallery_items
+		item_image
+		caption_item
+	items_infograficos
+		image_info_item
+
+*/
+
 get_template_part('includes/header');
 
-	while(have_posts()):the_post();
+	while(have_posts()):the_post(); ?>
+	
+	<header class="top-single-header">
+		<h1><?php the_title(); ?></h1>
+	</header>
+
+	<div class="main-content container">
+		<div class="row top-information">
+			<div class="col-md-6 side-profile">
+				<?php the_content(); ?>
+			</div>
+			<div class="col-md-6 side-profile">
+				
+				<?php if(get_field('logo_field')):?>
+					<figure class="logo-emp">
+						<img src="<?php the_field('logo_field'); ?>">
+					</figure>
+				<?php endif; ?>
+				
+				<?php if(get_field('items_contacto')):?>
+				<div class="contact-items">	
+					<h4>Contacto</h4>
+					<ul>
+						<?php while(have_rows('items_contacto')): the_row(); ?>
+							<li class="item-contact item-<?php the_sub_field('contact_type'); ?>">
+								<i class="fa fa-<?php the_sub_field('contact_type'); ?>" aria-hidden="true"></i>
+								<a href="<?php the_sub_field('vinculo_de_contacto'); ?>" target="_blank">
+									<?php the_sub_field('contact_text'); ?>
+								</a>
+							</li>
+						<?php endwhile;?>
+					</ul>
+					<?php if(get_field('url_page_field')):?>
+						<a href="<?php the_field('url_page_field')?>" class="btn btn-yellow" target="_blank">Ver página <i class="fa fa-play" aria-hidden="true"></i></a>
+					<?php endif; ?>	
+				</div>
+				<?php endif; ?>
+			</div>	
+		</div>
+		<div class="row gallery-contact">
+			<?php if(get_field('gallery_items')):?>
+
+				<ul class="gallery-carousel">
+					<?php while(have_rows('gallery_items')): the_row(); ?>
+						<li class="item-gallery" style="background-image: url(<?php the_sub_field('item_image'); ?>)">
+							<?php if(get_sub_field('caption_item')):?>
+							<div class="caption-slide">
+								<?php the_sub_field('caption_item'); ?>
+							</div>
+						<?php endif; ?>
+						</li>
+					<?php endwhile;?>
+				</ul>
+
+			<?php endif; ?>
+		</div>
+		<div class="row single-infographics">
+			<?php if(get_field('items_infograficos')):?>
+				<?php while(have_rows('items_infograficos')): the_row(); ?>
+					<div class="col-md-6 side-infographs">
+						<img src="<?php the_sub_field('image_info_item'); ?>" alt="<?php the_title(); ?>">
+					</div>
+				<?php endwhile;?>
+			<?php endif; ?>
+		</div>
+	</div>
 
 
+	<?php
 	endwhile;
 
 get_template_part('includes/footer'); ?>
